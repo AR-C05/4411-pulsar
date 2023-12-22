577483832ece99682a2a2843472b549b6a189772
AR-C05 <62663359+AR-C05@users.noreply.github.com>
2023-12-21 20:54:04 -0500

    made the path a bit more modular (symbol for home ~ instead of actual path)

--- createTopicDefault.sh
+++ createTopicDefault.sh
@@ -1,1 +1,1 @@
-/home/arc/apache-pulsar-3.1.1/bin/pulsar-admin topics create persistent://public/default/test-topic
+~/apache-pulsar-3.1.1/bin/pulsar-admin topics create persistent://public/default/test-topic
--- perfProducer.sh
+++ perfProducer.sh
@@ -1,1 +1,1 @@
-/home/arc/apache-pulsar-3.1.1/bin/pulsar-perf produce -s 1000 -r 10000 -u pulsar://localhost:6650 persistent://public/default/test-topic
+~/apache-pulsar-3.1.1/bin/pulsar-perf produce -s 1000 -r 10000 -u pulsar://localhost:6650 persistent://public/default/test-topic
