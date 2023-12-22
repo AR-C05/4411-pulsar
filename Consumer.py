94ebc899f855aed9d2e627276a67d294fd3ae244
Gabby Niamat <gniamat@uwo.ca>
2023-12-21 22:28:49 -0500

    acknowledgment

--- Consumer.py
+++ Consumer.py
@@ -1,4 +1,4 @@
-from pulsar import Client, ConsumerType
+from pulsar import Client, ConsumerType, Message
 import signal
 import sys
 
@@ -7,7 +7,7 @@ topic = 'test-topic'
 subscription = 'test-subscription'
 
 client = Client(service_url)
-consumer = client.subscribe(topic, subscription_name=subscription, consumer_type=ConsumerType.Shared)
+consumer = client.subscribe(topic, subscription_name=subscription, consumer_type=ConsumerType.Shared, ack_timeout_ms=10000, negative_ack_redelivery_delay_ms=5000)
 
 def exit_handler(signal, frame):
     # to ensure an error-free exit
@@ -17,10 +17,27 @@ def exit_handler(signal, frame):
 
 signal.signal(signal.SIGINT, exit_handler)
 
+def process_message(msg: Message):
+    try:
+        # Process the message
+        print(f"Received message: {msg.data().decode('utf-8')}")
+        
+        # Simulate processing error for demonstration
+        if "error" in msg.data().decode('utf-8'):
+            raise ValueError("Simulated processing error")
+
+    except Exception as e:
+        # Handle processing errors
+        print(f"Error processing message: {e}")
+        
+        # Negative acknowledgment (NACK) to indicate a processing error
+        consumer.negative_acknowledge(msg)
+
 try:
     while True:
         msg = consumer.receive()  # Receive messages
-        print("Received message: %s" % msg.data())
-        consumer.acknowledge(msg)  # Acknowledge the message
+        if msg:
+            process_message(msg)
+
 except KeyboardInterrupt:
     exit_handler(signal.SIGINT, None)
--- Producer.py
+++ Producer.py
@@ -1,12 +1,19 @@
 from pulsar import Client, Message
+import time
 
 service_url = 'pulsar://localhost:6650'
 topic = 'test-topic'
 
 client = Client(service_url)
-producer = client.create_producer(topic)
+producer = client.create_producer(topic, batching_enabled=True)
 
 # Generate and send messages
 for i in range(1000):  # Change the number of messages as needed
     message = "Message {}".format(i)
     producer.send(message.encode('utf-8'))
+
+
+producer.flush()
+
+producer.close()
+client.close()
