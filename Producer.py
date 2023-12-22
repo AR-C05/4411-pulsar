9dd4f412c455b223e47daf240f1e6077ab05094d
AR-C05 <62663359+AR-C05@users.noreply.github.com>
2023-12-22 18:52:49 -0500

    1000 messages too few

--- Producer.py
+++ Producer.py
@@ -7,7 +7,7 @@ client = Client(service_url)
 producer = client.create_producer(topic)
 
 # Generate and send messages
-for i in range(1000):  # Change the number of messages as needed
+for i in range(10000):  # Change the number of messages as needed
     message = "Message {}".format(i)
     producer.send(message.encode('utf-8'))
 
